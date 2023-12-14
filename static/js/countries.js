a3906f7caebaec7714dc0bb276f1433d9d80691d
ChinmaiHK <chinmaihk3@gmail.com>
2023-12-14 11:20:22 +0530

    Translation webpage

---
+++ static/js/countries.js
@@ -0,0 +1,10 @@
+const countries = {
+    "en-GB": "English",
+    "kn-IN": "ಕನ್ನಡ",
+    "hi-IN": "हिंदी",
+    "gu-IN": "ગુજરાતી",//Gujarati
+    "bn-IN": "Bengali",//Bengali
+    "pa-IN": "ਪੰਜਾਬੀ",//Punjabi
+    "ta-LK": "தமிழ்",//Tamil
+    "te-IN": "తెలుగు",//Telugu
+}
